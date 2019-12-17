5fa723b43925c11ede3cd12ba45ff4178355258a
programador2r <programador@rufi.com>
2019-12-17 17:23:02 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace PruebasWindowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(211, 78);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(121, 97);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.TreeView1_AfterSelect);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(238, 308);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(430, 20);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Click += new System.EventHandler(this.ProgressBar1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(462, 139);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 75);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(369, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebasWindowsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+
+        }
+        private static void PopulateAsesoresTree(TreeView tree)
+        {
+            List<Asesor> asesores = new List<Asesor> {
+                new Asesor { Formiikd="Asesor391" ,Id="391"},
+                new Asesor { Formiikd="Asesor311" ,Id="311"},
+                new Asesor { Formiikd="Asesor321" ,Id="321"},
+                new Asesor { Formiikd="Asesor331" ,Id="331"},
+                new Asesor { Formiikd="Asesor351" ,Id="351"},
+                new Asesor { Formiikd="Asesor351" ,Id="361"},
+                new Asesor { Formiikd="Asesor301" ,Id="301"},
+                new Asesor { Formiikd="Asesor381" ,Id="381"},
+                new Asesor { Formiikd="Asesor390" ,Id="390"},
+                new Asesor { Formiikd="Asesor191" ,Id="191"}
+            };
+            List <AsesorArbol> arboles = new List<AsesorArbol>();
+            foreach(string sucursal in new string[]{"Sucursal1","Sucursal2","Sucursal3" })
+            {
+                TreeNode node = new TreeNode(sucursal);
+                node.Name = string.Format("{0}", sucursal);
+
+             
+
+                foreach (Asesor a in asesores)
+                {
+                    TreeNode children = new TreeNode(string.Format("{0}", a.Formiikd));
+                    children.Name = string.Format("{0}","XXXXXXXXXXXX");
+                    node.Nodes.Add(children);
+
+                }
+                tree.Nodes.Add(node);
+            }
+
+        }
+            private void TreeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void ProgressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+        
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+
+                for (int i=0;  i< progressBar1.Maximum; ++i)
+                {
+                progressBar1.Value = i;
+                Console.WriteLine(progressBar1);
+                }
+        }
+    }
+    class AsesorArbol
+    {
+        TreeNode nodo;
+        Asesor asesor;
+    }
+    class Asesor
+    {
+        public string Formiikd { get; set; }
+        public string Id { get; set; }
+
+    }
+}
